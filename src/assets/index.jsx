875dd0154707f3f18af6a1ec710abf03fbdf513b
Ali Husnain Shah <a.h.shah322@gmail.com>
2025-11-15 23:44:52 +0500

    Added new Projects and Experiences

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -2,96 +2,100 @@ import logo from "./images/logo.png";
 import bannerImg from "./images/bannerImg.png";
 import projectOne from "./images/projects/projectOne.png";
 import projectTwo from "./images/projects/projectTwo.jpg";
-import projectThree from "./images/projects/projectThree.jpeg";
+import projectThree from "./images/projects/projectThree.jpg";
 import testimonialOne from "./images/testmonial/testimonialOne.png";
 import testimonialTwo from "./images/testmonial/testimonialTwo.png";
 import quote from "./images/testmonial/quote.png";
 import contactImg from "./images/contact/contactImg.png";
-import logoLight from './images/ahshah-light-small.png';
-import logoDark from './images/ahshah-dark-small.png';
-import myImage from './images/self.png';
+import logoLight from "./images/ahshah-light-small.png";
+import logoDark from "./images/ahshah-dark-small.png";
+import myImage from "./images/self.png";
 
 // Skill Import
-import css from './tech/css.png';
-import docker from './tech/docker.png';
-import figma from './tech/figma.png';
-import git from './tech/git.png';
-import html from './tech/html.png';
-import javascript from './tech/javascript.png';
-import nodejs from './tech/nodejs.png';
-import reactjs from './tech/reactjs.png';
-import redux from './tech/redux.png';
-import tailwind from './tech/tailwind.png';
-import typescript from './tech/typescript.png';
-import graphql from './tech/graphql.png';
-import postgresql from './tech/postgresql.png';
-import rubyrails from './tech/rubyrails.png';
-import csharp from './tech/csharp.png';
-import angular from './tech/angular.png';
-import netcore from './tech/net-core.png';
-import aspmvc from './tech/asp-mvc.png';
-import asp from './tech/asp.png';
-import jquery from './tech/jquery.png';
-import mssql from './tech/mssql.png';
-import blazor from './tech/blazor.png';
-import mudblazor from './tech/mudblazor.png';
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import graphql from "./tech/graphql.png";
+import postgresql from "./tech/postgresql.png";
+import rubyrails from "./tech/rubyrails.png";
+import csharp from "./tech/csharp.png";
+import python from "./tech/python.png";
+import angular from "./tech/angular.png";
+import netcore from "./tech/net-core.png";
+import aspmvc from "./tech/asp-mvc.png";
+import asp from "./tech/asp.png";
+import jquery from "./tech/jquery.png";
+import mssql from "./tech/mssql.png";
+import blazor from "./tech/blazor.png";
+import mudblazor from "./tech/mudblazor.png";
 
 // Resume Import
-import download from './icons/download.png';
-import downloadHover from './icons/downloadHover.png';
-import resume from './icons/resume.png';
+import download from "./icons/download.png";
+import downloadHover from "./icons/downloadHover.png";
+import resume from "./icons/resume.png";
 
 // Company Import
-import apksoft from './company/apksoft.png';
-import dolphindigital from './company/dolphin-digital.png';
-import hybridtechpK from './company/hybrid-tech-pk.png';
+import apksoft from "./company/apksoft.png";
+import dolphindigital from "./company/dolphin-digital.png";
+import hybridtechpK from "./company/hybrid-tech-pk.png";
+import qllmsoft from "./company/qllmsoft.png";
 
 export {
-  logo,
-  logoLight,
-  logoDark,
-  bannerImg,
-  projectOne,
-  projectTwo,
-  projectThree,
-  testimonialOne,
-  testimonialTwo,
-  quote,
-  contactImg,
-  myImage,
+	logo,
+	logoLight,
+	logoDark,
+	bannerImg,
+	projectOne,
+	projectTwo,
+	projectThree,
+	testimonialOne,
+	testimonialTwo,
+	quote,
+	contactImg,
+	myImage,
 
-  // Skill Import
-  css,
-  docker,
-  figma,
-  git,
-  html,
-  javascript,
-  nodejs,
-  reactjs,
-  redux,
-  tailwind,
-  typescript,
-  graphql,
-  postgresql,
-  rubyrails,
-  csharp,
-  angular,
-  netcore,
-  aspmvc,
-  asp,
-  jquery,
-  mssql,
-  blazor,
-  mudblazor,
-  
-  // Resume Import
-  download,
-  downloadHover,
-  resume,
+	// Skill Import
+	css,
+	docker,
+	figma,
+	git,
+	html,
+	javascript,
+	nodejs,
+	reactjs,
+	redux,
+	tailwind,
+	typescript,
+	graphql,
+	postgresql,
+	rubyrails,
+	csharp,
+	python,
+	angular,
+	netcore,
+	aspmvc,
+	asp,
+	jquery,
+	mssql,
+	blazor,
+	mudblazor,
 
-  // Company Import
-  apksoft,
-  dolphindigital,
-  hybridtechpK,
+	// Resume Import
+	download,
+	downloadHover,
+	resume,
+
+	// Company Import
+	apksoft,
+	dolphindigital,
+	hybridtechpK,
+	qllmsoft,
 };
